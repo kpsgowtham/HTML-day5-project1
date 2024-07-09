368ae502f20bfb14c462404bdde816a2c7470c5d
GOWTHAM P <kpsgowtham04@gmail.com>
2024-07-09 15:10:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,2 @@
+document.getElementById('off').addEventListener('click',function(){document.getElementById("image").src="https://cdn.pixabay.com/photo/2017/05/05/17/21/bulb-2287759_1280.jpg"});
+document.getElementById('on').addEventListener('click',function(){document.getElementById("image").src="https://comepbstatic.imgix.net/led_lightbulb.jpg?fm=pjpg&auto=compress"});
